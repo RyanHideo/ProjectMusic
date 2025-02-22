5ee8b1e37a6414583174a8ca5fd1d7d7bf4c9640
Oogakidev <ryanhougaske@gmail.com>
2025-02-22 10:26:26 -0400

    comecando o layout

---
+++ src/App2.jsx
@@ -0,0 +1,46 @@
+//rafce
+import React from "react";
+
+const App2 = () => {
+  return <div>App2</div>;
+};
+
+export default App2;
+
+//Arrow function
+// const App2 = () => <h1>Ola, Mundo!</h1>;
+
+//nomeação de components
+// PascalCase
+
+//Nomeacao de Variavel, Funcao...
+//camelCase
+
+//export default, posso importar com qualquer nome e sem chaves
+//export sem default, so posso importar entre chaves e com o nome exportado
+
+//Self closing tag
+// <Header></Header>
+// <Header/>
+
+// Element.style {
+//   background-color: antiquewhite;
+//   padding: 20px;
+//   margin: 20px;
+//   border: solid 5px black;
+//   /* box-sizing: content-box; */
+//   width: 500;
+// }
+
+// Nomeacao de classes em CSS
+// Metodologia BEM
+// Blocks é um componente
+// Elements faz parte de um bloco
+// Modifiers modifica o elemento
+// bloco__elemento--modificador
+// header
+// header__link
+// header__link--small
+// Nomes compostos sao separados por -
+
+//Tag vazia em React se chama Fragment ou Fragmento
